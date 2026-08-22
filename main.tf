aeae0a3df198a0a02e9899723f8c71a633a78602
waleedasif2244 <waleedasif2244@gmail.com>
2026-08-23 01:37:38 +0500

    aws work

--- main.tf
+++ main.tf
@@ -5,9 +5,22 @@ provider "aws" {
 }
 
 resource "aws_vpc" "waleed_vpc" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block = "10.0.0.0/24"
   
   tags = {
     Name = "waleed_vpc"
   }
+}
+
+# creting ec2
+
+resource "aws_instance" "web" {
+    ami = "ami-0c7217cdde317cfec"
+    instance_type ="t2.micro"
+tags={
+      Name = "waleed_instance" 
+      Environment= "DEV"
+
+}
+
 }
